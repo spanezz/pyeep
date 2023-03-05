209a421ec94c92a0ac551d5b612f655ce1d77b9e
Enrico Zini <enrico@enricozini.org>
2023-03-05 11:54:22 +0100

    Added delta list implementation to schedule timed events synced to integer frame counts

---
+++ pyeep/deltalist.py
@@ -0,0 +1,64 @@
+from __future__ import annotations
+
+from collections import deque
+from typing import TypeVar, Generic
+
+
+class Event:
+    def __init__(self, frame_delay: int = 0):
+        self.frame_delay: int = frame_delay
+
+
+EventType = TypeVar("EventType", bound=Event)
+
+
+class DeltaList(Generic[EventType]):
+    """
+    Delta list indexing a queue of events by the delay (in frames) at which
+    they are scheduled to happen
+    """
+    def __init__(self):
+        self.events: deque[EventType] = deque()
+
+    def add_event(self, event: EventType):
+        """
+        Enqueue an event at its frame_delay position.
+        """
+        if not self.events:
+            self.events.append(event)
+            return
+
+        insert_index: int | None = None
+        for idx, evt in enumerate(self.events):
+            if event.frame_delay < evt.frame_delay:
+                evt.frame_delay -= event.frame_delay
+                insert_index = idx
+                break
+            else:
+                event.frame_delay -= evt.frame_delay
+
+        match insert_index:
+            case None:
+                self.events.append(event)
+            case 0:
+                self.events.appendleft(event)
+            case _:
+                self.events.insert(idx, event)
+
+    def clock_tick(self, frames: int) -> list[EventType]:
+        """
+        Advance the delta list clock by the given number of frames.
+
+        Get a list with the events that happen in this clock tick, sorted by
+        frame delay from the start of the clock tick.
+        """
+        res: list[EventType] = []
+        while self.events and self.events[0].frame_delay < frames:
+            evt = self.events.popleft()
+            frames -= evt.frame_delay
+            if res:
+                evt.frame_delay += res[-1].frame_delay
+            res.append(evt)
+        if self.events:
+            self.events[0].frame_delay -= frames
+        return res
---
+++ tests/test_deltalist.py
@@ -0,0 +1,72 @@
+from __future__ import annotations
+
+import unittest
+
+from pyeep.deltalist import Event, DeltaList
+
+
+class TestEvent(Event):
+    def __init__(self, name: str, *args, **kw):
+        super().__init__(*args, **kw)
+        self.name = name
+
+
+class TestDeltaList(unittest.TestCase):
+    def test_empty(self):
+        dl = DeltaList()
+
+        self.assertEqual(dl.clock_tick(100), [])
+        self.assertEqual(dl.clock_tick(100), [])
+        self.assertEqual(dl.clock_tick(0), [])
+        self.assertEqual(dl.clock_tick(100), [])
+
+    def test_insert_start(self):
+        dl = DeltaList()
+
+        dl.add_event(TestEvent("first"))
+        dl.add_event(TestEvent("second"))
+        dl.add_event(TestEvent("third"))
+
+        self.assertEqual([evt.name for evt in dl.events], ["first", "second", "third"])
+
+    def test_add_get(self):
+        dl = DeltaList()
+
+        dl.add_event(TestEvent("second", 100))
+        dl.add_event(TestEvent("first", 10))
+        dl.add_event(TestEvent("fourth", 1000))
+        dl.add_event(TestEvent("zero", 0))
+        dl.add_event(TestEvent("third", 800))
+
+        self.assertEqual(
+                [(evt.name, evt.frame_delay) for evt in dl.events],
+                [("zero", 0),
+                 ("first", 10),
+                 ("second", 90),
+                 ("third", 700),
+                 ("fourth", 200)])
+
+        self.assertEqual(
+                [(evt.name, evt.frame_delay) for evt in dl.clock_tick(50)],
+                [("zero", 0),
+                 ("first", 10)])
+
+        self.assertEqual(
+                [(evt.name, evt.frame_delay) for evt in dl.events],
+                [("second", 50),
+                 ("third", 700),
+                 ("fourth", 200)])
+
+        self.assertEqual(
+                [(evt.name, evt.frame_delay) for evt in dl.clock_tick(100)],
+                [("second", 50)])
+
+        self.assertEqual(
+                [(evt.name, evt.frame_delay) for evt in dl.events],
+                [("third", 650),
+                 ("fourth", 200)])
+
+        self.assertEqual(
+                [(evt.name, evt.frame_delay) for evt in dl.clock_tick(1000)],
+                [("third", 650),
+                 ("fourth", 850)])
