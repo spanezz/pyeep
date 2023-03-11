7f2ca0ad3e717a64747f3558aee91eb24f6cc59c
Enrico Zini <enrico@enricozini.org>
2023-03-11 01:14:58 +0100

    Refactored note synth a bit, and prototype a sine wave note

--- pyeep/midisynth.py
+++ pyeep/midisynth.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import math
 from collections import deque
 from typing import Callable, Type
 
@@ -15,43 +16,48 @@ class Note:
         self.samplerate = samplerate
         self.dtype = dtype
         self.next_events: deque[mido.Message] = deque()
+        self.last_attenuation: float = 0
 
     def add_event(self, msg: mido.Message):
         self.next_events.append(msg)
 
-    def generate(self, frames: int) -> numpy.ndarray:
-        raise NotImplementedError()
-
-
-class OnOff(Note):
-    def __init__(self, note: int, samplerate: int, dtype):
-        super().__init__(note, samplerate, dtype)
-        self.last_value: float = 0
-
-    def generate(self, frame_time: int, frames: int) -> numpy.ndarray | None:
+    def get_events(self, frame_time: int, frames: int) -> list[mido.Message]:
         """
-        Return `frames` samples at the given frame time.
+        Dequeue the events we want to process.
 
-        If it returns None, it means this note is completely turned off
+        Update last_attenuation if some events need to be skipped
         """
-        # Dequeue the events we want to process
         events: list[mido.Message] = []
         while self.next_events and self.next_events[0].time < frame_time + frames:
             msg = self.next_events.popleft()
             if msg.time < frame_time:
                 match msg.type:
                     case "note_on":
-                        self.last_value = msg.velocity / 127.0
+                        self.last_attenuation = msg.velocity / 127.0
                     case "note_off":
-                        self.last_value = 0
+                        self.last_attenuation = 0
             else:
                 events.append(msg)
+        return events
+
+    def generate(self, frames: int) -> numpy.ndarray:
+        raise NotImplementedError()
+
+
+class OnOff(Note):
+    def generate(self, frame_time: int, frames: int) -> numpy.ndarray | None:
+        """
+        Return `frames` samples at the given frame time.
+
+        If it returns None, it means this note is completely turned off
+        """
+        events = self.get_events(frame_time, frames)
 
         if not events and not self.next_events:
-            if self.last_value == 0:
+            if self.last_attenuation == 0:
                 return None
             # No events: continue from last known value
-            return numpy.full(frames, self.last_value, dtype=self.dtype)
+            return numpy.full(frames, self.last_attenuation, dtype=self.dtype)
 
         sample: numpy.ndarray = numpy.zeros(0, dtype=self.dtype)
         for msg in events:
@@ -59,15 +65,56 @@ class OnOff(Note):
             match msg.type:
                 case "note_off":
                     if offset > len(sample):
-                        sample = numpy.concatenate((sample, numpy.full(offset - len(sample), self.last_value)))
-                    self.last_value = 0
+                        sample = numpy.concatenate((sample, numpy.full(offset - len(sample), self.last_attenuation)))
+                    self.last_attenuation = 0
                 case "note_on":
                     if offset > len(sample):
-                        sample = numpy.concatenate((sample, numpy.full(offset - len(sample), self.last_value)))
-                    self.last_value = msg.velocity / 127.0
+                        sample = numpy.concatenate((sample, numpy.full(offset - len(sample), self.last_attenuation)))
+                    self.last_attenuation = msg.velocity / 127.0
 
         if frames > len(sample):
-            sample = numpy.concatenate((sample, numpy.full(frames - len(sample), self.last_value)))
+            sample = numpy.concatenate((sample, numpy.full(frames - len(sample), self.last_attenuation)))
+
+        return sample
+
+
+class Sine(Note):
+    def __init__(self, *args, **kw):
+        super().__init__(*args, **kw)
+        self.freq = 440.0 * math.exp2((self.note - 69) / 12)
+        self.factor = self.freq * 2.0 * numpy.pi / self.samplerate
+
+    def generate(self, frame_time: int, frames: int) -> numpy.ndarray | None:
+        events = self.get_events(frame_time, frames)
+
+        if not events and not self.next_events:
+            if self.last_attenuation == 0:
+                return None
+            # No events: continue from last known value
+            x = numpy.arange(frame_time, frame_time + frames)
+            return numpy.sin(x * self.factor) * self.last_attenuation
+
+        sample: numpy.ndarray = numpy.zeros(frames, dtype=self.dtype)
+        last_offset = 0
+        for msg in events:
+            offset = msg.time - frame_time
+            match msg.type:
+                case "note_off":
+                    if offset > last_offset:
+                        x = numpy.arange(frame_time + last_offset, frame_time + offset)
+                        sample[last_offset:offset] = numpy.sin(x * self.factor) * self.last_attenuation
+                    self.last_attenuation = 0
+                    last_offset = offset
+                case "note_on":
+                    if offset > last_offset:
+                        x = numpy.arange(frame_time + last_offset, frame_time + offset)
+                        sample[last_offset:offset] = numpy.sin(x * self.factor) * self.last_attenuation
+                    self.last_attenuation = msg.velocity / 127.0
+                    last_offset = offset
+
+        if frames > last_offset:
+            x = numpy.arange(frame_time + last_offset, frame_time + frames)
+            sample[last_offset:frames] = numpy.sin(x * self.factor) * self.last_attenuation
 
         return sample
 
