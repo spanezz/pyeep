27a354d638ab5921bdcecbe0ab68895fe0b141c3
Enrico Zini <enrico@enricozini.org>
2023-04-29 16:21:42 +0200

    Added infrastructure to manage input devices

---
+++ pyeep/inputs.py
@@ -0,0 +1,123 @@
+from __future__ import annotations
+
+import inspect
+from typing import Type
+
+from .app import Message, Component
+from .gtk import Gio, GLib, Gtk, GtkComponent
+
+
+class InputSetActive(Message):
+    """
+    Activate/deactivate an input
+    """
+    def __init__(self, *, input: "Input", value: bool, **kwargs):
+        super().__init__(**kwargs)
+        self.input = input
+        self.value = value
+
+    def __str__(self) -> str:
+        return super().__str__() + f"(input={self.input}, value={self.value})"
+
+
+class InputSetMode(Message):
+    """
+    Set the behaviour mode for an input
+    """
+    def __init__(self, *, input: "Input", mode: str, **kwargs):
+        super().__init__(**kwargs)
+        self.input = input
+        self.mode = mode
+
+    def __str__(self) -> str:
+        return super().__str__() + f"(input={self.input}, mode={self.mode})"
+
+
+class Input(Component):
+    """
+    Generic base for component managing inputs
+    """
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.mode = self.mode_default
+
+    @property
+    def description(self) -> str:
+        return self.name
+
+    def get_input_controller(self) -> Type["InputController"]:
+        return InputController
+
+    def is_active(self) -> bool:
+        raise NotImplementedError(f"{self.__class__.__name__}._is_active not implemented")
+
+    def mode_default(self):
+        """
+        Default input behaviour
+        """
+        pass
+
+
+class InputController(GtkComponent):
+    """
+    User interface side for an input (controller and view)
+    """
+    def __init__(self, *, input: Input, **kwargs):
+        kwargs.setdefault("name", "input_model_" + input.name)
+        super().__init__(**kwargs)
+        self.input = input
+
+        self.active = Gio.SimpleAction.new_stateful(
+                name=self.name.replace("_", "-") + "-active",
+                parameter_type=None,
+                state=GLib.Variant.new_boolean(self.input.active))
+        self.active.connect("activate", self.on_activate)
+        self.hub.app.gtk_app.add_action(self.active)
+
+        self.modes = Gtk.ListStore(str, str)
+        for name, value in inspect.getmembers(self.input, inspect.ismethod):
+            if not name.startswith("mode_"):
+                continue
+            self.modes.append([name[5:], inspect.getdoc(value)])
+
+    def is_active(self) -> bool:
+        return self.active.get_state().get_boolean()
+
+    def on_activate(self, action, parameter):
+        new_state = not self.active.get_state().get_boolean()
+        self.active.set_state(GLib.Variant.new_boolean(new_state))
+        self.send(InputSetActive(input=self.input, value=new_state))
+
+    def on_mode_changed(self, combo):
+        tree_iter = combo.get_active_iter()
+        if tree_iter is not None:
+            model = combo.get_model()
+            mode = model[tree_iter][0]
+            self.send(InputSetMode(input=self.input, mode=mode))
+
+    def build(self) -> Gtk.Box:
+        """
+        Build the input view
+        """
+        grid = Gtk.Grid()
+
+        label_name = Gtk.Label(label=self.input.description)
+        label_name.wrap = True
+        label_name.set_halign(Gtk.Align.START)
+        grid.attach(label_name, 0, 0, 1, 1)
+
+        active = Gtk.CheckButton(label="Active")
+        active.set_action_name("app." + self.active.get_name())
+        grid.attach(active, 0, 1, 1, 1)
+
+        if len(self.modes) > 1:
+            modes = Gtk.ComboBox(model=self.modes)
+            modes.set_id_column(0)
+            renderer = Gtk.CellRendererText()
+            modes.pack_start(renderer, True)
+            modes.add_attribute(renderer, "text", 1)
+            modes.set_active_id("default")
+            modes.connect("changed", self.on_mode_changed)
+            grid.attach(modes, 0, 2, 1, 1)
+
+        return grid
