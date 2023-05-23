7366a0099b008b39841844f4c6b1af1ae8b8f783
Enrico Zini <enrico@enricozini.org>
2023-05-23 13:59:24 +0200

    Automatically add JackComponent instances to be called in the JACK process callback

--- pyeep/component/jack.py
+++ pyeep/component/jack.py
@@ -1,9 +1,23 @@
 from __future__ import annotations
 
+import threading
+
 import jack
 
-from ..messages import Shutdown
+from ..messages import NewComponent, Shutdown
+from .base import Component
 from .aio import AIOComponent
+
+
+class JackComponent(Component):
+    """
+    Component that gets called by the JACK realtime process function
+    """
+    def jack_process(self, frames: int):
+        """
+        JACK process function, running in JACK's realtime thread
+        """
+        raise NotImplementedError(f"{self.__class__.__name__}.jack_process not implemented")
 
 
 class Jack(AIOComponent):
@@ -11,17 +25,41 @@ class Jack(AIOComponent):
         super().__init__(**kwargs)
         self.jack_name = jack_name
         self.jack_client = jack.Client(self.jack_name)
-        self.jack_client.set_process_callback(self.on_process)
+        self.jack_client.set_process_callback(self.jack_process)
         self.samplerate = self.jack_client.samplerate
+        self.jack_components: list["JackComponent"] = []
+        self.jack_components_lock = threading.Lock()
 
-    def on_process(self, frames: int):
+    def jack_process(self, frames: int):
         """
         JACK process function, running in JACK's realtime thread
         """
-        raise NotImplementedError(f"{self.__class__.__name__}.on_process not implemented")
-    # def on_process(self, frames: int):
-    #     for c in self.components.values():
-    #         c.on_process(frames)
+        with self.jack_components_lock:
+            for c in self.jack_components:
+                c.jack_process(frames)
+
+    def add_jack_component(self, component: JackComponent) -> None:
+        """
+        Add a JACK component to be called in the JACK process function.
+
+        The function is idempotent: adding a comnponent multiple times is the
+        same as adding it only once
+        """
+        with self.jack_components_lock:
+            if component not in self.process_callbacks:
+                self.process_callbacks.append(component)
+
+    def remove_jack_component(self, component: JackComponent) -> None:
+        """
+        Remove a JACK component.
+
+        If the comnponent is not present, nothing will happen
+        """
+        with self.process_callbacks_lock:
+            try:
+                self.jack_components.remove(component)
+            except ValueError:
+                pass
 
     async def run(self) -> None:
         """
@@ -29,6 +67,9 @@ class Jack(AIOComponent):
         """
         with self.jack_client:
             while True:
-                match await self.next_message():
+                match (msg := await self.next_message()):
                     case Shutdown():
                         break
+                    case NewComponent():
+                        if isinstance(msg.src, JackComponent):
+                            self.add_jack_component(msg.src)
