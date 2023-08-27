06a74319adfd0350112c802f5edfbf1a4054f951
Enrico Zini <enrico@enricozini.org>
2023-08-27 18:23:29 +0100

    More more Jack moved to pyeep

---
+++ pyeep/component/subprocess.py
@@ -0,0 +1,153 @@
+from __future__ import annotations
+
+import asyncio
+import json
+import tempfile
+from pathlib import Path
+from typing import Sequence
+
+from ..messages.component import Shutdown
+from ..messages.jsonable import Jsonable
+from .aio import AIOComponent
+
+
+# See https://bugs.python.org/issue43884
+# It looks like asyncio is currently not very good at doing subprocess pipes
+# reading from stdout, and it might be better to create and pass a socket
+# instead, as argument
+
+class TopComponent(AIOComponent):
+    """
+    Component that manages a child process
+
+    This is the controller side of a BottomComponent
+    """
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.proc: asyncio.subprocess.Process | None = None
+        self.read_messages_task: asyncio.Task | None = None
+        self.returncode: int | None = None
+        self.workdir: Path | None = None
+        self.server: asyncio.Server | None = None
+        self.reader: asyncio.StreamReader | None = None
+        self.writer: asyncio.StreamWriter | None = None
+
+    def get_commandline(self) -> Sequence[str]:
+        raise NotImplementedError(f"{self.__class__.__name__}.get_commandline not implemented")
+
+    async def _read_messages(self):
+        try:
+            while (line := await self.reader.readline()):
+                jsonable = json.loads(line)
+                cls = Jsonable.jsonable_class(jsonable)
+                if cls is None:
+                    continue
+
+                jsonable["src"] = self
+
+                try:
+                    msg = cls(**jsonable)
+                except Exception as e:
+                    self.logger.error("cannot instantiate message: %s", e)
+                    continue
+
+                self.send(msg)
+        finally:
+            self.receive(Shutdown())
+
+    async def _killer(self):
+        attempt = 0
+        while self.proc.returncode is None:
+            if attempt < 10:
+                self.proc.terminate()
+            else:
+                self.proc.kill()
+            attempt += 1
+            await asyncio.sleep(0.1)
+
+    async def _terminate_process(self):
+        try:
+            await asyncio.gather(self._killer(), self.proc.wait())
+
+            self.returncode = self.proc.returncode
+        finally:
+            self.proc = None
+
+    async def on_server_connect(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+        self.reader = reader
+        self.writer = writer
+        self.read_messages_task = asyncio.create_task(self._read_messages())
+
+    async def run(self):
+        with tempfile.TemporaryDirectory() as workdir_str:
+            self.workdir = Path(workdir_str)
+            self.server = await asyncio.start_unix_server(
+                    self.on_server_connect,
+                    path=self.workdir / "socket")
+
+            self.proc = await asyncio.create_subprocess_exec(
+                    *self.get_commandline())
+
+            while True:
+                match (msg := await self.next_message()):
+                    case Shutdown():
+                        if self.proc is not None:
+                            await self._terminate_process()
+                        break
+                    case _:
+                        if msg.src != self and self.writer is not None:
+                            line = json.dumps(msg.as_jsonable()) + "\n"
+                            self.writer.write(line.encode())
+                            await self.writer.drain()
+
+
+class BottomComponent(AIOComponent):
+    """
+    Component that interfaces with a controller program.
+
+    This is the remote controlled by a TopComponent
+    """
+    def __init__(self, path: Path, **kwargs):
+        super().__init__(**kwargs)
+        self.path = path
+        self.read_messages_task: asyncio.Task | None = None
+        self.returncode: int | None = None
+        self.workdir: Path | None = None
+        self.reader: asyncio.StreamReader | None = None
+        self.writer: asyncio.StreamWriter | None = None
+
+    async def _read_messages(self):
+        try:
+            while (line := await self.reader.readline()):
+                jsonable = json.loads(line)
+                cls = Jsonable.jsonable_class(jsonable)
+                if cls is None:
+                    continue
+
+                jsonable["src"] = self
+
+                try:
+                    msg = cls(**jsonable)
+                except Exception as e:
+                    self.logger.error("cannot instantiate message: %s", e)
+                    continue
+
+                print("RECEIVED", msg)
+                self.send(msg)
+        finally:
+            self.receive(Shutdown())
+
+    async def run(self):
+        self.reader, self.writer = await asyncio.open_unix_connection(path=self.path)
+        self.read_messages_task = asyncio.create_task(self._read_messages())
+
+        while True:
+            match (msg := await self.next_message()):
+                case Shutdown():
+                    break
+                case _:
+                    if msg.src != self:
+                        print("SEND", msg)
+                        line = json.dumps(msg.as_jsonable()) + "\n"
+                        self.writer.write(line.encode())
+                        await self.writer.drain()
---
+++ tests/test_subprocess.py
@@ -0,0 +1,130 @@
+from __future__ import annotations
+
+import asyncio
+import json
+import os
+import shlex
+import tempfile
+import unittest
+from pathlib import Path
+
+from pyeep.component.subprocess import SubprocessComponent
+from pyeep.messages.component import DeviceScanRequest, Shutdown
+from pyeep.messages.message import Message
+
+
+class MockHub:
+    def __init__(self):
+        self.messages_sent: list[Message] = []
+
+    def _running_in_hub(self):
+        return True
+
+    def send(self, msg: Message):
+        self.messages_sent.append(msg)
+
+
+class TestSubprocess(unittest.IsolatedAsyncioTestCase):
+    def setUp(self):
+        self.workdir = Path(self.enterContext(tempfile.TemporaryDirectory()))
+
+    async def test_start_stop(self):
+        scriptfile = self.workdir / "script"
+        scriptfile.write_text("#!/bin/sh\nsleep 100")
+        scriptfile.chmod(0o755)
+
+        class Comp(SubprocessComponent):
+            def get_commandline(self):
+                return [scriptfile]
+
+        comp = Comp(hub=MockHub())
+        comp_task = asyncio.create_task(comp.run())
+
+        while comp.proc is None:
+            await asyncio.sleep(0.1)
+        pid = comp.proc.pid
+
+        comp.receive(Shutdown())
+
+        await comp_task
+
+        with self.assertRaises(ProcessLookupError):
+            os.kill(pid, 0)
+
+    async def test_send(self):
+        outfile = self.workdir / "output"
+
+        scriptfile = self.workdir / "script"
+        scriptfile.write_text(f"#!/bin/sh\nnc -U $1 > {outfile}")
+        scriptfile.chmod(0o755)
+
+        class Comp(SubprocessComponent):
+            def get_commandline(self):
+                return [scriptfile, self.workdir / "socket"]
+
+        comp = Comp(hub=MockHub())
+        comp_task = asyncio.create_task(comp.run())
+
+        while comp.proc is None:
+            await asyncio.sleep(0.1)
+
+        comp.receive(DeviceScanRequest(duration=3.14, ts=12.34))
+
+        comp.receive(Shutdown())
+
+        await comp_task
+
+        output = outfile.read_text()
+        self.assertNotEqual(output, "")
+        parsed = json.loads(output)
+        self.assertEqual(parsed, {
+            '__class__': 'DeviceScanRequest',
+            '__module__': 'pyeep.messages.component',
+            'dst': None,
+            'duration': 3.14,
+            'name': 'devicescanrequest',
+            'src': None,
+            'ts': 12.34})
+
+    async def test_receive(self):
+        payload = {
+            '__class__': 'DeviceScanRequest',
+            '__module__': 'pyeep.messages.component',
+            'dst': None,
+            'duration': 3.14,
+            'name': 'devicescanrequest',
+            'src': None,
+            'ts': 12.34,
+        }
+        encoded = shlex.quote(json.dumps(payload))
+
+        outfile = self.workdir / "output"
+
+        scriptfile = self.workdir / "script"
+        scriptfile.write_text(f"#!/bin/sh\necho {encoded} | nc -U $1 > {outfile}")
+        scriptfile.chmod(0o755)
+
+        class Comp(SubprocessComponent):
+            def get_commandline(self):
+                return [scriptfile, self.workdir / "socket"]
+
+        hub = MockHub()
+        comp = Comp(hub=hub)
+        comp_task = asyncio.create_task(comp.run())
+
+        while comp.proc is None:
+            await asyncio.sleep(0.1)
+
+        comp.receive(DeviceScanRequest(duration=3.14, ts=12.34))
+
+        comp.receive(Shutdown())
+
+        await comp_task
+
+        self.assertEqual(len(hub.messages_sent), 1)
+        msg = hub.messages_sent[0]
+        self.assertIsInstance(msg, DeviceScanRequest)
+        self.assertEqual(msg.duration, 3.14)
+        self.assertEqual(msg.ts, 12.34)
+
+        self.assertEqual(outfile.read_text(), "")
