73241a2f711978d325f44ab7624766b0ec901a5a
Enrico Zini <enrico@enricozini.org>
2026-04-12 19:27:38 +0200

    Disable numba jit

--- pyeep/synth.py
+++ pyeep/synth.py
@@ -1,13 +1,12 @@
-from __future__ import annotations
-
 import math
 
-import numba
+# import numba
+# from numba.experimental import jitclass
+
 import numpy
-from numba.experimental import jitclass
 
 
-class Wave():
+class Wave:
     """
     Base class for wave synthesizers
     """
@@ -15,18 +14,23 @@ class Wave():
     def wave(self, array: numpy.ndarray, freq: float) -> None:
         raise NotImplementedError(f"{self.__class__.__name__}.wave")
 
-    def synth(self, array: numpy.ndarray, freq: float, envelope: numpy.ndarray) -> None:
+    def synth(
+        self, array: numpy.ndarray, freq: float, envelope: numpy.ndarray
+    ) -> None:
         raise NotImplementedError(f"{self.__class__.__name__}.synth")
 
 
-@jitclass([
-    ("rate", numba.int32),
-    ("phase", numba.float64),
-])
+# @jitclass(
+#     [
+#         ("rate", numba.int32),
+#         ("phase", numba.float64),
+#     ]
+# )
 class SineWave(Wave):
     """
     Phase accumulation synthesis
     """
+
     # See https://www.gkbrk.com/wiki/PhaseAccumulator/
 
     def __init__(self, rate: int):
@@ -34,27 +38,38 @@ class SineWave(Wave):
         self.phase: float = 0.0
 
     def skip(self, frames: int, freq: float):
-        self.phase = (self.phase + frames * 2.0 * math.pi * freq / self.rate) % (2 * math.pi)
+        self.phase = (
+            self.phase + frames * 2.0 * math.pi * freq / self.rate
+        ) % (2 * math.pi)
 
     def wave(self, array: numpy.ndarray, freq: float) -> None:
         for i in range(len(array)):
-            self.phase = (self.phase + 2.0 * math.pi * freq / self.rate) % (2 * math.pi)
+            self.phase = (self.phase + 2.0 * math.pi * freq / self.rate) % (
+                2 * math.pi
+            )
             array[i] += math.sin(self.phase)
 
-    def synth(self, array: numpy.ndarray, freq: float, envelope: numpy.ndarray) -> None:
+    def synth(
+        self, array: numpy.ndarray, freq: float, envelope: numpy.ndarray
+    ) -> None:
         for i in range(len(array)):
-            self.phase = (self.phase + 2.0 * math.pi * freq / self.rate) % (2 * math.pi)
+            self.phase = (self.phase + 2.0 * math.pi * freq / self.rate) % (
+                2 * math.pi
+            )
             array[i] += math.sin(self.phase) * envelope[i]
 
 
-@jitclass([
-    ("rate", numba.int32),
-    ("phase", numba.float64),
-])
+# @jitclass(
+#     [
+#         ("rate", numba.int32),
+#         ("phase", numba.float64),
+#     ]
+# )
 class SawWave:
     """
     Phase accumulation synthesis
     """
+
     # See https://www.gkbrk.com/wiki/PhaseAccumulator/
 
     def __init__(self, rate: int):
@@ -66,27 +81,32 @@ class SawWave:
             self.phase = (self.phase + freq / self.rate) % 2
             array[i] += self.phase - 1
 
-    def synth(self, array: numpy.ndarray, freq: float, envelope: numpy.ndarray) -> None:
+    def synth(
+        self, array: numpy.ndarray, freq: float, envelope: numpy.ndarray
+    ) -> None:
         for i in range(len(array)):
             self.phase = (self.phase + freq / self.rate) % 2
             array[i] += (self.phase - 1) * envelope[i]
 
 
-@jitclass([
-    ("attack_level", numba.float64),
-    ("attack_time", numba.float64),
-    ("decay_time", numba.float64),
-    ("sustain_level", numba.float64),
-    ("release_time", numba.float64),
-])
+# @jitclass(
+#     [
+#         ("attack_level", numba.float64),
+#         ("attack_time", numba.float64),
+#         ("decay_time", numba.float64),
+#         ("sustain_level", numba.float64),
+#         ("release_time", numba.float64),
+#     ]
+# )
 class EnvelopeShape:
     def __init__(
-            self,
-            attack_level: float = 1.0,
-            attack_time: float = 0.1,
-            decay_time: float = 0.2,
-            sustain_level: float = 0.9,
-            release_time: float = 0.2):
+        self,
+        attack_level: float = 1.0,
+        attack_time: float = 0.1,
+        decay_time: float = 0.2,
+        sustain_level: float = 0.9,
+        release_time: float = 0.2,
+    ):
         self.attack_level = attack_level
         self.attack_time = attack_time
         self.decay_time = decay_time
@@ -94,41 +114,53 @@ class EnvelopeShape:
         self.release_time = release_time
 
 
-@jitclass([
-    ("start_frame", numba.int32),
-    ("rate", numba.int32),
-    ("velocity", numba.float64),
-    ("sustain_start", numba.int32),
-    ("release_frames", numba.int32),
-    ("release_start", numba.int32),
-    ("head", numba.float64[:]),
-    ("tail", numba.float64[:]),
-])
+# @jitclass(
+#     [
+#         ("start_frame", numba.int32),
+#         ("rate", numba.int32),
+#         ("velocity", numba.float64),
+#         ("sustain_start", numba.int32),
+#         ("release_frames", numba.int32),
+#         ("release_start", numba.int32),
+#         ("head", numba.float64[:]),
+#         ("tail", numba.float64[:]),
+#     ]
+# )
 class Envelope:
     shape: EnvelopeShape
 
     def __init__(
-            self,
-            shape: EnvelopeShape,
-            frame_time: int,
-            rate: int,
-            start_level: float = 0.0,
-            velocity: float = 1.0):
+        self,
+        shape: EnvelopeShape,
+        frame_time: int,
+        rate: int,
+        start_level: float = 0.0,
+        velocity: float = 1.0,
+    ):
         self.shape = shape
         self.start_frame = frame_time
         self.rate = rate
         self.velocity = velocity
-        self.sustain_start = round((self.shape.attack_time + self.shape.decay_time) * rate)
+        self.sustain_start = round(
+            (self.shape.attack_time + self.shape.decay_time) * rate
+        )
         self.release_frames = round(self.shape.release_time * rate)
         self.release_start: int = 0
         # Precomputed lead values
-        self.head = numpy.concatenate((
+        self.head = numpy.concatenate(
+            (
                 numpy.linspace(
-                    start_level, self.shape.attack_level * velocity,
-                    round(self.shape.attack_time * rate)),
+                    start_level,
+                    self.shape.attack_level * velocity,
+                    round(self.shape.attack_time * rate),
+                ),
                 numpy.linspace(
-                    self.shape.attack_level * velocity, self.shape.sustain_level * velocity,
-                    round(self.shape.decay_time * rate))))
+                    self.shape.attack_level * velocity,
+                    self.shape.sustain_level * velocity,
+                    round(self.shape.decay_time * rate),
+                ),
+            )
+        )
         self.tail: numpy.zeros(1)
 
     def release(self, frame_time: int) -> None:
@@ -159,7 +191,7 @@ class Envelope:
                 offset = elapsed - self.release_start
                 size = min(frames, len(self.tail))
                 # print(f"  r {offset=} {size=}")
-                return self.tail[offset:offset + size]
+                return self.tail[offset : offset + size]
         elif elapsed >= len(self.head):
             # Sustain
             if self.release_start == 0:
@@ -174,7 +206,7 @@ class Envelope:
             if self.release_start > 0:
                 size = min(size, self.release_start - elapsed)
             # print(f"  ad {elapsed=} {size=}")
-            return self.head[elapsed:elapsed + size]
+            return self.head[elapsed : elapsed + size]
 
     def generate(self, frame_time: int, frames: int) -> numpy.ndarray | None:
         res = numpy.zeros(frames)
@@ -189,7 +221,7 @@ class Envelope:
                 chunk = numpy.zeros(frames - start)
             else:
                 has_data = True
-            res[start:start+len(chunk)] = chunk
+            res[start : start + len(chunk)] = chunk
             start += len(chunk)
 
         return res
